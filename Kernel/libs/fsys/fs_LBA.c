03d8254f60183a2f058dea5d04d83036d68fadc8
Fedor Lapshin <42876964+FedorLap2006@users.noreply.github.com>
2019-02-09 15:35:48 +0300

    Update fs_LBA.c

--- Kernel/libs/fsys/fs_LBA.c
+++ Kernel/libs/fsys/fs_LBA.c
@@ -83,7 +83,7 @@
 // fat example ( 13h ) https://habrastorage.org/getpro/habr/post_images/0d6/275/777/0d627577730050dfa24baa9928b2a0d0.jpg
 
 int sys_write(LBA info){
-	byte func = FS_READ;
+	byte func = FS_WRITE;
 	int32 jsbits = info.dap.sector << (8*4);
 	int32 ssbits = info.dap.sector;
 	
@@ -156,5 +156,75 @@ int sys_write(LBA info){
 	asm volatile("popal"); // pop from stack all x32 regs
 }
 int sys_read(LBA info){
-	byte func = FS_WRITE;
+	byte func = FS_READ;
+	int32 jsbits = info.dap.sector << (8*4);
+	int32 ssbits = info.dap.sector;
+	
+	asm volatile("pushal"); // push to stack all x32 regs
+
+	///////////////////////////////////////////////////////////////////
+
+
+	// prepare data to interrput ( LBA call )
+	
+	asm volatile(
+		"push %%es\n"
+		"push %%bx"
+	); // prepare es,bx ( addr of buffer )
+
+	
+	
+	// use buf_ptr ptr as segment based ptr and mov 0 offset to bx
+	
+	asm(
+		"movl (%0),%%es" // corrected mov
+			:
+			: "m" (info.dap.buf)
+			:
+	);
+	asm(
+		"movl $0,%%bx"
+	);
+
+	///////////////////////////////////////////////////////////////////
+	
+	// general push to stack
+
+	
+	asm(
+		"push %0\n"
+			:
+			: "r" (ssbits)
+			:
+	); // push senior-bits of sector number
+
+	asm(
+		"push %0\n"
+			:
+			: "r" (jsbits)
+			:
+	); // push junior-bits of sector number	
+
+	asm("push %%es");
+	asm("push %%bx");
+	asm("push $%0": : "M" (info.dap.sector_count) :);
+	
+	asm("push $0x10");
+	
+	///////////////////////////////////////////////////////////////////	
+
+	// recovery used registers and clear stack
+
+	// ...
+
+	///////////////////////////////////////////////////////////////////
+	
+	asm("movl $%0,%%ah" : : "M" (func) : ); // prepare number of func 
+ 	asm("movl $%0,%%dl" : : "M" (info.hdrive) : ); // drive index ( from 0 )
+ 	asm("movl %%esp,%%esi"); // prepare DAP struct
+ 	
+	//////////////////////////////////////////////////////////////////
+	
+	asm("int $0x13"); // interrput ( LBA call )
+	asm volatile("popal"); // pop from stack all x32 regs
 }
