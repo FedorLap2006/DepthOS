f43270544c89304cd2d8ca866fb8629aa2eab65d
Jennifer Smith <jennifersmith94536@gmail.com>
2019-04-23 20:17:25 -0700

    idt: drop num_tochar
    The function is not used anywhere, drop it.
    
    Signed-off-by: Jennifer Smith <jennifersmith94536@gmail.com>

--- arch/idt.c
+++ arch/idt.c
@@ -127,32 +127,6 @@ void __idt_default_handler() {
 	console_write("hello!");
 }
 
-
-char num_tochar(int i) {
-	if ( i == 0 )
-	  return '0';
-	if ( i == 1 )
-	  return '1';	
-	if ( i == 2 )
-	  return '2';
-	if ( i == 3 )
-	  return '3';	
-	if ( i == 4 )
-	  return '4';	
-	if ( i == 5 )
-	  return '5';	
-	if ( i == 6 )
-	  return '6';
-	if ( i == 7 )
-	  return '7';	
-	if ( i == 8 )
-	  return '8';
-	if ( i == 9 )
-	  return '9';
-	return -1;	
-//	for( int i = sizeof(i) / 8; i > 0; i-- ) {
-}
-
 void irq_handler(regs_t r) {
 	/*console_write("\n");
 	console_write("ds:");
