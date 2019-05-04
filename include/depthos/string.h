f8b5e126d2b0927b0907615b17f2a2fa8365cc12
Jennifer Smith <jennifersmith94536@gmail.com>
2019-05-04 00:46:33 -0700

    lib/string: add memcmp, strcmp and strstr
    Signed-off-by: Jennifer Smith <jennifersmith94536@gmail.com>

--- include/depthos/string.h
+++ include/depthos/string.h
@@ -3,7 +3,10 @@
 #include <depthos/stddef.h>
 
 void *memchr(const void *p, int c, size_t n);
+int memcmp(const void *a, const void *b, size_t n);
 void *memcpy(void *dst, const void *src, size_t n);
 void *memset(void *p, int c, size_t n);
 char *strchr(const char *s, int c);
+int strcmp(const char *a, const char *b);
 size_t strlen(const char *s);
+char *strstr(const char *haystack, const char *needle);
