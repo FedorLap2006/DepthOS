801416d4781552e4d4d1ab428bed403ac15f6fab
nitroflap <fe.lap.prog@gmail.com>
2022-07-12 15:49:28 +0300

    refactor(sched,proc)!: rename and move task functions
    Make task functions naming consistent and move them to proc.c

--- kernel/proc.c
+++ kernel/proc.c
@@ -1,3 +1,4 @@
+#include <depthos/bitmap.h>
 #include <depthos/elf.h>
 #include <depthos/errno.h>
 #include <depthos/fs.h>
@@ -6,6 +7,144 @@
 #include <depthos/proc.h>
 #include <depthos/string.h>
 #include <depthos/syscall.h>
+
+void _task_init_kstack(struct task *task) {
+  task->kernel_stack = kmalloc(0x1000);
+  printk("kernel stack: %x\n", task->kernel_stack);
+  task->kernel_esp = task->kernel_stack + 0x1000;
+}
+
+static struct task *create_dummy_task() {
+  struct task *task = kmalloc(sizeof(struct task));
+  _task_init_kstack(task);
+  return task;
+}
+
+void task_setup_stack(struct task *task, void *stack) {
+  map_addr(task->pgd, stack, 1, true);
+  uint32_t *kstack = task->kernel_esp;
+  // memset(task->kernel_stack, 0, 0x1000 - 1);
+  uint32_t tmp = stack + 0x1000;
+#define PUSH(v) *--kstack = v
+  PUSH(0x23);              /* ss */
+  PUSH(tmp);               /* useresp */
+  PUSH(0x202);             /* eflags */
+  PUSH(0x1b);              /* cs */
+  PUSH(task->binfo.entry); /* eip */
+
+  PUSH(0x0);
+  PUSH(0);
+  PUSH(0);
+
+  PUSH(0x0);  /* eax */
+  PUSH(0x0);  /* ecx */
+  PUSH(0x0);  /* edx */
+  PUSH(0x0);  /* ebx */
+  PUSH(0x0);  /* esp */
+  PUSH(0x0);  /* ebp */
+  PUSH(0x0);  /* esi */
+  PUSH(0x0);  /* edi */
+  PUSH(0x23); /* ds */
+  PUSH(0x23); /* es */
+  PUSH(0x23); /* gs */
+  PUSH(0x23); /* fs */
+#undef PUSH
+  // get_current_pgd()[pde_index(VIRT_BASE - 0x1000)] = 0;
+  // activate_pgd(get_current_pgd());
+  task->regs = (struct registers *)kstack;
+  task->stack = kstack;
+}
+
+void task_setup_filetable(struct fs_node **ft) {
+  memset(ft, 0, sizeof(struct fs_node *) * TASK_FILETABLE_MAX);
+  struct fs_node *tty_file = vfs_open("/dev/tty0");
+  ft[0] = tty_file;
+  ft[1] = tty_file;
+}
+
+struct task *create_task(void *entry, pagedir_t pgd, bool do_stack,
+                         void *stack) {
+  struct task *task = create_dummy_task();
+  task->pgd = pgd;
+  task->binfo.entry = entry;
+  task_setup_stack(task, stack);
+  return task;
+}
+
+struct task *fork_task(struct task *parent) {
+  printk("============== forking task %s ================\n", parent->name);
+  extern bitmap_t kheap_bitmap;
+  struct task *task = create_dummy_task();
+  bitmap_dump_compact(&kheap_bitmap, 0, -1, 32);
+  task->name = strdup(parent->name);
+  task->state = parent->state;
+  task->thid = parent->thid;
+  task->pgd = clone_pgd(parent->pgd);
+  task->binfo = parent->binfo;
+  task->process = NULL;
+  task->sched_entry = NULL;
+  task->parent = parent;
+  task->filetable = kmalloc(sizeof(struct fs_node *) * TASK_FILETABLE_MAX);
+  printk("filetable: %p\n", task->filetable);
+  bitmap_dump_compact(&kheap_bitmap, 0, -1, 32);
+  printk("kernel_stack: %p\n", task->kernel_stack);
+  printk("kernel_stack: *%p\n", &task);
+  memcpy(task->filetable, parent->filetable,
+         sizeof(struct fs_node *) * TASK_FILETABLE_MAX);
+  printk("kernel_stack: %p\n", task->kernel_stack);
+  bitmap_dump_compact(&kheap_bitmap, 0, -1, 32);
+  memcpy(task->kernel_stack, parent->kernel_stack, 0x1000);
+  task->regs =
+      task->kernel_esp - (parent->kernel_esp - (uintptr_t)parent->regs);
+  task->regs->eax = 0;
+  return task;
+}
+
+struct task *create_kernel_task(void *entry, bool do_stack) {
+  struct task *task = create_dummy_task();
+  task->pgd = kernel_pgd;
+  if (!do_stack)
+    return;
+  uint32_t *stack = task->kernel_esp;
+  uint32_t tmp;
+  // stack -= 4;
+
+#define PUSH(v) *--stack = v
+  PUSH(0x10);  /* ss */
+  PUSH(0x0);   /* useresp */
+  PUSH(0x202); /* eflags */
+  PUSH(0x08);  /* cs */
+  PUSH(entry); /* eip */
+
+  // TODO: implement tracing for kernel tasks?
+  PUSH(0x0); /* trace_frame */
+  PUSH(0);   /* err_code */
+  PUSH(0);   /* int_num */
+
+  tmp = stack;
+  PUSH(0x0);  /* eax */
+  PUSH(0x0);  /* ecx */
+  PUSH(0x0);  /* edx */
+  PUSH(0x0);  /* ebx */
+  PUSH(tmp);  /* esp */
+  PUSH(0x0);  /* ebp */
+  PUSH(0x0);  /* esi */
+  PUSH(0x0);  /* edi */
+  PUSH(0x10); /* ds */
+  PUSH(0x10); /* es */
+  PUSH(0x10); /* gs */
+  PUSH(0x10); /* fs */
+
+#undef PUSH
+  task->regs = (struct registers *)stack;
+  task->stack = stack;
+  task->binfo.entry = entry;
+
+  task->filetable = kmalloc(sizeof(struct fs_node *) * TASK_FILETABLE_MAX);
+  task_setup_filetable(task->filetable);
+
+  return task;
+}
 
 pid_t current_pid = 0;
 static struct list free_pid;
@@ -35,6 +174,12 @@ struct task *thread_create(void *entry, void *stack, pagedir_t pgd,
   return thread;
 }
 
+void thread_destroy(struct task *thread) {
+  sched_remove(thread);
+  kfree(thread->pgd, 4096); // TODO: implement pgd freeing
+  kfree(thread, sizeof(*thread));
+}
+
 struct process *process_spawn(const char *filepath, struct process *parent) {
   struct fs_node *file = vfs_open(filepath);
   if (!file)
@@ -46,7 +191,7 @@ struct process *process_spawn(const char *filepath, struct process *parent) {
   process->state = PROCESS_STARTING;
   process->parent = parent;
 
-  struct task *main_thread = kmalloc(sizeof(struct task)); // TODO
+  struct task *main_thread = create_dummy_task();
   main_thread->thid = 0;
   // main_thread->parent = NULL; // TODO: remove
   main_thread->process = process;
@@ -54,6 +199,7 @@ struct process *process_spawn(const char *filepath, struct process *parent) {
   main_thread->sched_entry = NULL;
   main_thread->filetable = process->filetable =
       kmalloc(sizeof(struct fs_node *) * TASK_FILETABLE_MAX);
+  task_setup_filetable(main_thread->filetable);
 #if 0
   vfs_seek(file, 0);
   char *buffer = kmalloc(256);
@@ -64,10 +210,8 @@ struct process *process_spawn(const char *filepath, struct process *parent) {
 	  elf_load(main_thread, filepath);
   kfree(buffer, 256);
 #endif
-  _task_init(main_thread);
   elf_loadf(main_thread, file);
-  bootstrap_user_task(main_thread, true, VIRT_BASE - 0x1000);
-  setup_task_filetable(main_thread->filetable);
+  task_setup_stack(main_thread, VIRT_BASE - 0x1000);
   sched_add(main_thread);
 
   process->threads = list_create();
@@ -86,11 +230,10 @@ void process_kill(struct process *process) {
     }
   list_foreach(process->threads, entry) {
     struct task *thread = list_item(entry, struct task *);
-    if (current_task->process == process && thread->thid == current_task->thid)
+    if (current_task == thread)
       current_task->state = TASK_DYING;
     else
-      sched_remove(list_item(entry, struct task *));
-    kfree(thread, sizeof(*thread));
+      thread_destroy(thread);
   }
   if (!process->parent)
     panicf("Cannot kill init process");
@@ -105,7 +248,7 @@ void process_kill(struct process *process) {
 DECL_SYSCALL0(exit) { process_kill(current_task->process); }
 DECL_SYSCALL0(fork) {
   idt_disable_hwinterrupts();
-  struct task *main_thread = create_task_fork(current_task);
+  struct task *main_thread = fork_task(current_task);
   struct process *process = kmalloc(sizeof(struct process));
   main_thread->process = process;
   process->pid = generate_pid();
@@ -159,7 +302,7 @@ DECL_SYSCALL1(thkill, thid_t, thid) {
         current_task->state = TASK_DYING;
         reschedule();
       } else
-        sched_remove(list_item(entry, struct task *));
+        thread_destroy(list_item(entry, struct task *));
     }
   }
 }
@@ -174,15 +317,18 @@ DECL_SYSCALL0(thjoin) {
 DECL_SYSCALL1(execve, const char *, file) {
   bool v = elf_probe(file);
   if (!v) {
-    printk("file: %s\n", file);
-    return -EINVAL;
+    printk("file: %s\n", file);    return -EINVAL;
   }
   extern struct task *current_task;
   // printk("AAAAAAAAAAAAAAAAA\n");
-  setup_task_filetable(current_task->filetable);
+  task_setup_filetable(current_task->filetable);
   elf_load(current_task, file);
+  current_task->process->filepath = current_task->name;
   struct fs_node *tty_file = vfs_open("/dev/tty0");
   current_task->filetable[0] = tty_file;
   current_task->filetable[1] = tty_file;
-  elf_exec(current_task);
+  task_setup_stack(current_task, VIRT_BASE - PAGE_SIZE);
+  reschedule_to(current_task);
+  // map_addr(current_task->pgd, VIRT_BASE - PAGE_SIZE, 1, true, false);
+  // elf_exec(current_task);
 }
--- kernel/sched.c
+++ kernel/sched.c
@@ -9,46 +9,23 @@
 #include <depthos/syscall.h>
 #include <depthos/tss.h>
 
-// struct tasklist {
-//   struct tasklist *next;
-//   struct tasklist *prev;
-//   struct task *task;
-// };
-
-// static struct tasklist *tasklist_head;
-// static struct tasklist *tasklist_top;
-// static struct tasklist *tasklist_current;
 static struct list *tasklist;
 static struct list_entry *current_entry;
 struct task *current_task;
 
-bool __preempt_enabled = false;
+static bool __sched_prof = false;
+static bool __preempt_enabled = false;
 
 void preempt_enable() { __preempt_enabled = true; }
 void preempt_disable() { __preempt_enabled = false; }
+void sched_prof_enable() { __sched_prof = true; }
+void sched_prof_disable() { __sched_prof = false; }
 
 void __idle_thread() {
   preempt_enable();
-  // extern void init(void);
-  // struct task *init_task = create_kernel_task(init, true);
-  // init_task->name = "";
-  // sched_add(init_task);
-  // printk("================ YIELDING ================");
 
-  for (;;) {
-    // __asm__ volatile("int $0x80" : : "a"(4), "b"(1), "c"('a'));
+  for (;;)
     __asm__ volatile("int $0x30");
-  }
-  // __asm__ volatile("int $0x80" ::"a"(5));
-}
-
-void __th1() {
-  for (;;)
-    ;
-}
-void __th2() {
-  for (;;)
-    ;
 }
 
 void dump_task(struct task *t) {
@@ -91,17 +68,6 @@ void dump_tasks() {
   list_foreach(tasklist, task) { dump_task(list_item(task, struct task *)); }
 }
 
-void _task_init(struct task *task) {
-  task->kernel_stack = kmalloc(0x1000);
-  task->kernel_esp = task->kernel_stack + 0x1000;
-}
-
-static struct task *create_dummy_task() {
-  struct task *task = kmalloc(sizeof(struct task));
-  _task_init(task);
-  return task;
-}
-
 void sched_init() {
   idt_disable_hwinterrupts();
   tasklist = list_create();
@@ -129,18 +95,17 @@ void sched_init() {
 void reschedule_to(struct task *t) {
   current_task->state = TASK_RUNNING;
   current_task = t;
-#if 0
-  if (t->process)
-    printk("switching to thread no. %d of %s\n", t->thid, t->process->filepath);
-  else
-    printk("switching to %s\n", t->name);
-  dump_task(current_task);
-#endif
+  if (__sched_prof) {
+    if (t->process)
+      printk("switching to %s[%d]\n", t->process->filepath, t->thid);
+    else
+      printk("switching to %s\n", t->name);
+    dump_task(current_task);
+  }
   tss_set_stack(t->kernel_esp);
 
   if (t->pgd)
     activate_pgd(t->pgd);
-  // printk("=============== switch task: %s\n", t->name);
   pic_eoi(0x20);
   __asm__ volatile("movl %0, %%esp;"
                    "jmp intr_exit;" ::"r"(current_task->regs));
@@ -158,170 +123,12 @@ void reschedule(void) {
 
   struct list_entry *next = pick_next_task();
   if (current_task->state == TASK_DYING) {
-    printk("cleaning up dying task\n");
-    sched_remove(current_task);
-    kfree(current_task, sizeof(*current_task));
+    klogf("task %s is dying\n", current_task->name);
+    thread_destroy(current_task);
   }
 
   current_entry = next;
   reschedule_to(list_item(current_entry, struct task *));
-}
-
-void bootstrap_user_task(struct task *task, bool do_stack, void *stack) {
-  _task_init(task);
-  if (!do_stack)
-    return;
-  uintptr_t page = pmm_alloc(1);
-  if (!page) {
-    kfree(task, sizeof(struct task));
-    return NULL;
-  }
-#if 0
-  pmm_dump_compact();
-  pagedir_t current_pgd = get_current_pgd();
-  klogf("directory:");
-  for (int j = 0; j < 1024; j++) {
-    if (!current_pgd[j])
-      continue;
-    klogf("[0x%x]: %x:", j * 4096 * 1024, current_pgd[j]);
-    for (int k = 0; k < 1024; k++) {
-      pageinfo_t pg =
-          parse_page(get_page(current_pgd, j * 4096 * 1024 + k * 4096));
-      klogf("\t[0x%x]: 0x%x", j * 4096 * 1024 + k * 4096, pg.frame);
-    }
-  }
-  map_addr_phys(current_pgd, VIRT_BASE - 0x1000, page, 1);
-  klogf("directory:");
-  for (int j = 0; j < 1024; j++) {
-    if (!current_pgd[j])
-      continue;
-    klogf("[0x%x]: %x:", j * 4096 * 1024, current_pgd[j]);
-    for (int k = 0; k < 1024; k++) {
-      pageinfo_t pg =
-          parse_page(get_page(current_pgd, j * 4096 * 1024 + k * 4096));
-      klogf("\t[0x%x]: 0x%x", j * 4096 * 1024 + k * 4096, pg.frame);
-    }
-  }
-#endif
-
-#if 0
-  map_addr_phys(get_current_pgd(), VIRT_BASE - 0x1000, page, 1);
-  activate_pgd(get_current_pgd());
-  if (task->pgd)
-    map_addr_phys(task->pgd, VIRT_BASE - 0x1000, page, 1);
-#endif
-  map_addr_phys(task->pgd, stack, page, 1);
-  if (task->pgd == get_current_pgd())
-    activate_pgd(get_current_pgd());
-  uint32_t *kstack = task->kernel_esp;
-  // memset(task->kernel_stack, 0, 0x1000 - 1);
-  uint32_t tmp = stack + 0x1000;
-#define PUSH(v) *--kstack = v
-  PUSH(0x23);              /* ss */
-  PUSH(tmp);               /* useresp */
-  PUSH(0x202);             /* eflags */
-  PUSH(0x1b);              /* cs */
-  PUSH(task->binfo.entry); /* eip */
-
-  PUSH(0x0);
-  PUSH(0);
-  PUSH(0);
-
-  PUSH(0x0);  /* eax */
-  PUSH(0x0);  /* ecx */
-  PUSH(0x0);  /* edx */
-  PUSH(0x0);  /* ebx */
-  PUSH(0x0);  /* esp */
-  PUSH(0x0);  /* ebp */
-  PUSH(0x0);  /* esi */
-  PUSH(0x0);  /* edi */
-  PUSH(0x23); /* ds */
-  PUSH(0x23); /* es */
-  PUSH(0x23); /* gs */
-  PUSH(0x23); /* fs */
-#undef PUSH
-  // get_current_pgd()[pde_index(VIRT_BASE - 0x1000)] = 0;
-  // activate_pgd(get_current_pgd());
-  task->regs = (struct registers *)kstack;
-  task->stack = kstack;
-}
-
-void setup_task_filetable(struct fs_node *ft) {
-
-  memset(ft, 0, sizeof(struct fs_node *) * TASK_FILETABLE_MAX);
-}
-
-struct task *create_task(void *entry, pagedir_t pgd, bool do_stack,
-                         void *stack) {
-  struct task *task = create_dummy_task();
-  task->pgd = pgd;
-  task->binfo.entry = entry;
-  bootstrap_user_task(task, do_stack, stack);
-  return task;
-}
-
-// FIXME: kernel tasks cannot use fork
-struct task *create_task_fork(struct task *parent) {
-  struct task *task = kmalloc(sizeof(struct task));
-  *task = *parent;
-  task->parent = parent;
-  task->name = strdup(parent->name);
-  task->pgd = clone_pgd(parent->pgd);
-  task->filetable = kmalloc(sizeof(struct fs_node *) * TASK_FILETABLE_MAX);
-  memcpy(task->filetable, parent->filetable,
-         sizeof(struct fs_node *) * TASK_FILETABLE_MAX);
-  _task_init(task);
-  memcpy(task->kernel_stack, parent->kernel_stack, 0x1000);
-  task->regs =
-      task->kernel_esp - (parent->kernel_esp - (uintptr_t)parent->regs);
-  task->regs->eax = 0;
-  return task;
-}
-
-struct task *create_kernel_task(void *entry, bool do_stack) {
-  struct task *task = create_dummy_task();
-  task->pgd = kernel_pgd;
-  if (!do_stack)
-    return;
-  uint32_t *stack = task->kernel_esp;
-  uint32_t tmp;
-  // stack -= 4;
-
-#define PUSH(v) *--stack = v
-  PUSH(0x10);  /* ss */
-  PUSH(0x0);   /* useresp */
-  PUSH(0x202); /* eflags */
-  PUSH(0x08);  /* cs */
-  PUSH(entry); /* eip */
-
-  // TODO: implement tracing for kernel tasks?
-  PUSH(0x0); /* trace_frame */
-  PUSH(0);   /* err_code */
-  PUSH(0);   /* int_num */
-
-  tmp = stack;
-  PUSH(0x0);  /* eax */
-  PUSH(0x0);  /* ecx */
-  PUSH(0x0);  /* edx */
-  PUSH(0x0);  /* ebx */
-  PUSH(tmp);  /* esp */
-  PUSH(0x0);  /* ebp */
-  PUSH(0x0);  /* esi */
-  PUSH(0x0);  /* edi */
-  PUSH(0x10); /* ds */
-  PUSH(0x10); /* es */
-  PUSH(0x10); /* gs */
-  PUSH(0x10); /* fs */
-
-#undef PUSH
-  task->regs = (struct registers *)stack;
-  task->stack = stack;
-  task->binfo.entry = entry;
-
-  task->filetable = kmalloc(sizeof(struct fs_node *) * TASK_FILETABLE_MAX);
-  setup_task_filetable(task->filetable);
-
-  return task;
 }
 
 void sched_add(struct task *task) {
@@ -337,16 +144,11 @@ void sched_remove(struct task *task) {
 }
 
 void sched_yield(regs_t *r) {
-  // printk("before yield: \n");
-  // dump_task(current_task);
   current_task->regs = r;
-  // printk("after yield: \n");
-  // dump_task(current_task);
   reschedule();
 }
 
 void sched_ticker(regs_t *r) {
-  // trace(0, -1);
   idt_disable_hwinterrupts();
   current_task->running_time++;
   current_task->running_time_sched = 0;
