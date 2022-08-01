2c233febd8f000b51071908642b75170c5822016
FedorLap2006 <fe.lap.prog@gmail.com>
2022-08-02 00:20:22 +0300

    chore(tss): drop unnecessary segment population
    Drop unnecessary population of cs, ds, es, fs, gs and ss segments.

--- arch/x86/tss.c
+++ arch/x86/tss.c
@@ -1,16 +1,12 @@
+#include <depthos/string.h>
 #include <depthos/tss.h>
+#include <depthos/x86/gdt.h>
 struct tss kernel_tss __align(4096);
 
 void tss_init() {
   memset(&kernel_tss, 0, sizeof(struct tss));
-  kernel_tss.cs = 0x0b;
-  kernel_tss.ds = 0x13;
-  kernel_tss.es = 0x13;
-  kernel_tss.fs = 0x13;
-  kernel_tss.gs = 0x13;
-  kernel_tss.ss = 0x13;
 }
 void tss_set_stack(void *stack) {
-  kernel_tss.ss0 = 0x10;
+  kernel_tss.ss0 = GDT_KERNEL_DATA_SEL;
   kernel_tss.esp0 = stack;
 }
