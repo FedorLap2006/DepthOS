bfc39efaf43b349822d9c574f07c49bde01943f0
Fedor Lapshin <fe.lap.prog@gmail.com>
2024-03-04 15:22:44 +0300

    feat: add and correct existing general, file and value related errnos

--- include/depthos/errno.h
+++ include/depthos/errno.h
@@ -2,8 +2,24 @@
 
 #include <depthos/stdtypes.h>
 
-#define ENOENT -1
-#define EINVAL -2
-#define ENIMPL -3
+
+#define EINVAL 1
+#define EOVERFLOW 2
+#define E2BIG 3
+#define ENIMPL 4
+#define ENOSYS ENIMPL
+#define EFAULT 5
+#define EACCES 6
+#define EPERM 7
+#define ENOENT 8
+#define EBADF 9
+#define EAGAIN 10
+#define ENOTDIR 11
+#define ENOIOCTL 12
+#define ENOTTY ENOIOCTL
+#define EMFILE 13
+#define ENOEXEC 14
+#define EEXIST 15
+#define ENOMEM 16
 
 extern int errno;
